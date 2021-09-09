aca0db563dc2328285fe6022fbc13b6f2e2b2def
buzzysin <buzzysin@users.noreply.github.com>
2021-09-09 02:23:36 +0100

    ✔️ barebones cli functionality

--- packages/cli/.babelrc.js
+++ packages/cli/.babelrc.js
@@ -1,4 +1,19 @@
 /** @type {import("@babel/core").TransformOptions} */
 module.exports = {
   extends: "../../babel.config.js",
+  overrides: [
+    {
+      plugins: [
+        [
+          "babel-plugin-module-resolver",
+          {
+            alias: {
+              "^@buzzybot/(\\w+)$": "../../packages/\\1/bin",
+              "^@buzzybot/(\\w+)/(.+)$": "../../packages/\\1/bin/\\2",
+            },
+          },
+        ],
+      ],
+    },
+  ],
 };
--- packages/cli/src/buzzybot.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "commander";
-import { version } from "../package.json";
-import { buzzybotInit } from "./buzzybot-init";
-
-const program = new Command("buzzybot");
-
-program.version(version).addCommand(buzzybotInit, { isDefault: true });
-
-program.parse();
